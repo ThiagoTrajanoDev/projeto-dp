549b2df1c73ce945c88320b9b860f0d3b3dd4b59
Thiago <ttf1880@gmail.com>
2025-07-10 19:46:03 -0300

    feat: cria repositorys

---
+++ src/main/java/br/com/projetoDP/repository/BotaoRepositoryImpl.java
@@ -0,0 +1,9 @@
+package br.com.projetoDP.repository;
+
+import br.com.projetoDP.domain.Botao;
+import br.com.projetoDP.utils.IBaseRepositoryBridge;
+
+public class BotaoRepositoryImpl implements IBaseRepositoryBridge<Botao> {
+
+
+}
---
+++ src/main/java/br/com/projetoDP/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package br.com.projetoDP.repository;
+
+import br.com.projetoDP.domain.UserObserver;
+import br.com.projetoDP.utils.IBaseRepositoryBridge;
+
+public class UserRepository implements IBaseRepositoryBridge<UserObserver> {
+}
