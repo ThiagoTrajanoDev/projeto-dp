dab6eac1eaebb1236e547e82e296cfc0dbd1ead3
Mayara Silva <mayara.dnascimentos@gmail.com>
2025-07-10 21:29:01 -0300

    chore: adiciona anotação nos repositórios

--- src/main/java/br/com/projetoDP/repository/BotaoRepositoryImpl.java
+++ src/main/java/br/com/projetoDP/repository/BotaoRepositoryImpl.java
@@ -2,7 +2,9 @@ package br.com.projetoDP.repository;
 
 import br.com.projetoDP.domain.Botao;
 import br.com.projetoDP.utils.IBaseRepositoryBridge;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class BotaoRepositoryImpl implements IBaseRepositoryBridge<Botao> {
 
 
--- src/main/java/br/com/projetoDP/repository/UserRepository.java
+++ src/main/java/br/com/projetoDP/repository/UserRepository.java
@@ -2,6 +2,8 @@ package br.com.projetoDP.repository;
 
 import br.com.projetoDP.domain.UserObserver;
 import br.com.projetoDP.utils.IBaseRepositoryBridge;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class UserRepository implements IBaseRepositoryBridge<UserObserver> {
 }
