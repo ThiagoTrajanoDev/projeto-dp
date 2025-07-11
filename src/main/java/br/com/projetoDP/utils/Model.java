1c372c0a864b6b79021ca82746a7d29326348479
Thiago <ttf1880@gmail.com>
2025-07-10 21:45:30 -0300

    refactor: altera nivel de acesso de atributo por conta do quarkus

--- src/main/java/br/com/projetoDP/utils/Model.java
+++ src/main/java/br/com/projetoDP/utils/Model.java
@@ -4,10 +4,9 @@ package br.com.projetoDP.utils;
 import jakarta.persistence.*;
 
 @MappedSuperclass
-@Access(AccessType.PROPERTY)
 public class Model {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    public Long id;
 }
