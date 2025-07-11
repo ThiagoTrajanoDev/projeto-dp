5f2a79e974980e981f80b05fe28216af74a81461
Thiago <ttf1880@gmail.com>
2025-07-10 21:44:05 -0300

    feat: implementa poc da stack do botao

--- src/main/java/br/com/projetoDP/domain/Botao.java
+++ src/main/java/br/com/projetoDP/domain/Botao.java
@@ -4,6 +4,10 @@ package br.com.projetoDP.domain;
 import br.com.projetoDP.utils.Model;
 import jakarta.persistence.Entity;
 
+import java.time.LocalDateTime;
+
 @Entity
 public class Botao extends Model {
+    public String local;
+    public LocalDateTime horario;
 }
--- src/main/java/br/com/projetoDP/service/BotaoFacade.java
+++ src/main/java/br/com/projetoDP/service/BotaoFacade.java
@@ -1,13 +1,15 @@
 package br.com.projetoDP.service;
 
 import br.com.projetoDP.domain.Botao;
+import br.com.projetoDP.domain.UserObserver;
 import br.com.projetoDP.repository.BotaoRepositoryImpl;
 import br.com.projetoDP.utils.BaseService;
+import br.com.projetoDP.utils.NotificacaoStrategy;
 import br.com.projetoDP.utils.Observable;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.persistence.PersistenceException;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 
@@ -16,21 +18,42 @@ public class BotaoFacade extends BaseService<Botao> implements Observable {
 
 
     private final BotaoRepositoryImpl repository;
+    private final UserProxy userProxy;
+    private final NotificacaoStrategy notificacaoStrategy;
 
-    protected BotaoFacade(BotaoRepositoryImpl repository) {
+    protected BotaoFacade(BotaoRepositoryImpl repository, UserProxy userProxy, NotificacaoStrategy notificacaoStrategy) {
         super(repository);
         this.repository = repository;
+        this.userProxy = userProxy;
+        this.notificacaoStrategy = notificacaoStrategy;
     }
 
     @GET
+    @Path("/history")
     @Produces(MediaType.APPLICATION_JSON)
     @Override
     public List<Botao> findAll() {
         return repository.listAll();
     }
 
-    @Override
-    public void notificar() {
-
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response pushButton(Botao botao){
+        try{
+            repository.persist(botao);
+            notificar(botao);
+            return Response.accepted().build();
+        } catch (PersistenceException e) {
+            throw new PersistenceException(e.getMessage());
+        }
     }
+
+    @Override
+    public void notificar(Botao botao) {
+        List<UserObserver> users =  userProxy.findAll();
+        users.forEach(user -> System.out.println("Enviando mensagem para " + user.email + "./n Mensagem: " + NotificacaoStrategy.getNotificacao(user,botao)));
+    }
+
+
 }
