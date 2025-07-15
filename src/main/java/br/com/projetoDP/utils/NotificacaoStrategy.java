fa0dc09cc8a4515e46e8cd03641834a30b3ff095
Mayara Silva <mayara.dnascimentos@gmail.com>
2025-07-14 21:15:19 -0300

    feat: refatora estratégia de notificação para mensagens específicas de usuários

--- src/main/java/br/com/projetoDP/utils/NotificacaoStrategy.java
+++ src/main/java/br/com/projetoDP/utils/NotificacaoStrategy.java
@@ -1,20 +1,23 @@
 package br.com.projetoDP.utils;
 
 import br.com.projetoDP.domain.Botao;
-import br.com.projetoDP.domain.Notificacao;
 import br.com.projetoDP.domain.UserObserver;
 
 public class NotificacaoStrategy {
 
-    public static String getNotificacao(UserObserver user, Botao botao){
-        if(user.tipo.equals(Type.ALUNO)){
-            return new Notificacao.NotificacaoBuilder().user(user).botao(botao).buildNotificacaoAluno();
-        }
-        else if(user.tipo.equals(Type.PROFESSOR)){
-            return new Notificacao.NotificacaoBuilder().user(user).botao(botao).buildNotificacaoProfessor();
-        }
-        else{
-            return new Notificacao.NotificacaoBuilder().user(user).botao(botao).buildNotificacaoServidor();
+    public static String gerarMensagem(UserObserver user, Botao botao) {
+        switch (user.tipo) {
+            case PROFESSOR:
+                return "Professor " + user.nome + ", foi acionado o botão de pânico localizado no local: " +
+                        botao.local + " às " + botao.horario + ".\nLeve seus alunos à segurança seguindo o protocolo de treinamento.";
+            case ALUNO:
+                return "Caro aluno, foi acionado o botão de pânico localizado no local: " +
+                        botao.local + ".\nComunique seu professor, siga as instruções passadas por ele e mantenha a calma.";
+            case SERVIDOR:
+                return "Servidor " + user.nome + ", foi acionado o botão de pânico localizado no local: " +
+                        botao.local + " às " + botao.horario + ".\nSiga o protocolo de segurança, chame as autoridades competentes e mantenha a calma.";
+            default:
+                return "Notificação: botão de pânico acionado.";
         }
     }
 }
