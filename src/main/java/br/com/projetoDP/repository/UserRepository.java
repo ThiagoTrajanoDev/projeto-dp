ee67d6a06ce0d3b944500deaa5f4877f6fc4980a
Mayara Silva <mayara.dnascimentos@gmail.com>
2025-07-15 19:34:13 -0300

    feat: implementa método findByEmail no UserRepository

--- src/main/java/br/com/projetoDP/repository/UserRepository.java
+++ src/main/java/br/com/projetoDP/repository/UserRepository.java
@@ -3,7 +3,24 @@ package br.com.projetoDP.repository;
 import br.com.projetoDP.domain.UserObserver;
 import br.com.projetoDP.utils.IBaseRepositoryBridge;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
 
 @ApplicationScoped
 public class UserRepository implements IBaseRepositoryBridge<UserObserver> {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public UserObserver findByEmail(String email) {
+        try {
+            return em.createQuery("SELECT u FROM UserObserver u WHERE u.email = :email", UserObserver.class)
+                    .setParameter("email", email)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
 }
