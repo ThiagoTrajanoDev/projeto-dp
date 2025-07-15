469d97773e0cf2ee7ed773d2cfb2b5ca09eeb6c5
Mayara Silva <mayara.dnascimentos@gmail.com>
2025-07-14 21:15:42 -0300

    feat: adiciona repositório para gerenciamento de notificações

---
+++ src/main/java/br/com/projetoDP/repository/NotificacaoRepository.java
@@ -0,0 +1,9 @@
+package br.com.projetoDP.repository;
+
+import br.com.projetoDP.domain.Notificacao;
+import br.com.projetoDP.utils.IBaseRepositoryBridge;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class NotificacaoRepository implements IBaseRepositoryBridge<Notificacao> {
+}
