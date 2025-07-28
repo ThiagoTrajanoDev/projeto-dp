cf15d4d4dee4baac7c871a2f55654c2922abbbce
Thiago <ttf1880@gmail.com>
2025-07-28 17:22:44 -0300

    feat: cria UserProxy

--- src/main/java/br/com/projetoDP/service/UserProxy.java
+++ src/main/java/br/com/projetoDP/service/UserProxy.java
@@ -1,10 +1,16 @@
 package br.com.projetoDP.service;
 
-import br.com.projetoDP.entity.UserObserver;
+import br.com.projetoDP.dto.LoginRequest;
+import br.com.projetoDP.dto.UserObserver;
+import br.com.projetoDP.entity.User;
 import br.com.projetoDP.repository.UserRepository;
 import br.com.projetoDP.utils.BaseService;
 import br.com.projetoDP.utils.Role;
+import io.smallrye.common.annotation.Blocking;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.persistence.PersistenceException;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -12,7 +18,7 @@ import jakarta.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/user")
-public class UserProxy extends BaseService<UserObserver> {
+public class UserProxy extends BaseService<User> {
 
     private final UserRepository repository;
 
@@ -21,81 +27,86 @@ public class UserProxy extends BaseService<UserObserver> {
         this.repository = repository;
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
+
     @Override
-    public List<UserObserver> findAll() {
+    public List<User> findAll() {
         return repository.listAll();
     }
 
+    @GET
+    @PermitAll
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<UserObserver> findAllObservers() {
+        List<User> users = repository.listAll();
+        return users.stream()
+                .map(user -> new UserObserver(user.getNome(),
+                        user.getMatricula(),
+                        user.getEmail(),
+                        user.getRole(),
+                        user.getTipo())).toList();
+    }
+
     @POST
-    @Path("/{role}")
+    @RolesAllowed("ADMIN")
+    @Path("/create")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createUser(UserObserver user, @PathParam("role") Role role) {
-        if (role != Role.ADMIN) {
-            return Response.status(Response.Status.FORBIDDEN)
-                    .entity("Apenas usuário com papel ADMIN pode criar outros usuários.").build();
-        }
+    @Transactional
+    public Response createUser(
+            User user
+    ) {
         try {
             repository.persist(user);
-            return Response.status(Response.Status.CREATED).entity(user).build();
+            UserObserver userObserver = new UserObserver(
+                    user.getNome(),
+                    user.getMatricula(),
+                    user.getEmail(),
+                    user.getRole(),
+                    user.getTipo()
+            );
+            return Response.status(Response.Status.CREATED).entity(userObserver).build();
         } catch (PersistenceException e) {
             return Response.serverError().entity("Erro ao criar usuário: " + e.getMessage()).build();
         }
     }
 
     @DELETE
-    @Path("/{id}/{role}")
-    public Response deleteUser(@PathParam("id") Long id, @PathParam("role") Role role) {
-        if (role != Role.ADMIN) {
-            return Response.status(Response.Status.FORBIDDEN)
-                    .entity("Apenas usuário com papel ADMIN pode remover outros usuários.").build();
-        }
-        UserObserver user = repository.findById(id);
+    @RolesAllowed("ADMIN")
+    @Path("/{id}")
+    @Transactional
+    public Response deleteUser(
+            @PathParam("id") Long id
+    ) {
+        User user = repository.findById(id);
         if (user == null) {
             return Response.status(Response.Status.NOT_FOUND)
-                    .entity("Usário não encontrado.").build();
+                    .entity("Usuário não encontrado").build();
+        }
+        if (user.getRole().equals(Role.ADMIN.name())) {
+            return Response.status(Response.Status.FORBIDDEN)
+                    .entity("Não é possível deletar um usuário com papel ADMIN").build();
         }
         try {
             repository.delete(user);
-            return Response.noContent().build();
+            return Response.ok("Usuário deletado com sucesso").build();
         } catch (PersistenceException e) {
-            return Response.serverError().entity("Erro ao remover usuário: " + e.getMessage()).build();
+            return Response.serverError().entity("Erro ao deletar usuário: " + e.getMessage()).build();
         }
     }
 
     @POST
-    @Path("/register/{role}")
+    @PermitAll
+    @Path("/login")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response register(UserObserver user, @PathParam("role") Role role) {
-        if (role != Role.ADMIN && role != Role.MONITOR) {
-            return Response.status(Response.Status.FORBIDDEN)
-                    .entity("Apenas usuários ADMIN ou MONITOR podem realizar cadastro.").build();
-        }
-        if (repository.findByEmail(user.getEmail()) != null) {
-            return Response.status(Response.Status.CONFLICT)
-                    .entity("E-mail já registrado").build();
-        }
-        repository.persist(user);
-        return Response.status(Response.Status.CREATED).entity(user).build();
-    }
-
-    @POST
-    @Path("/login/{role}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response login(UserObserver credentials, @PathParam("role") Role role) {
-        if (role != Role.ADMIN && role != Role.MONITOR) {
-            return Response.status(Response.Status.FORBIDDEN)
-                    .entity("Apenas ADMIN ou MONITOR podem realizar login").build();
-        }
-        UserObserver user = repository.findByEmail(credentials.getEmail());
-        if (user == null || !user.getPassword().equals(credentials.getPassword())) {
+    @Blocking
+    public Response login(LoginRequest credentials) {
+        User user = repository.findByEmail(credentials.getEmail());
+        if (user == null || !user.getPassword().equals(credentials.getSenha())) {
             return Response.status(Response.Status.UNAUTHORIZED)
                     .entity("E-mail ou senha inválidos").build();
         }
+
         return Response.ok(user).build();
     }
 }
