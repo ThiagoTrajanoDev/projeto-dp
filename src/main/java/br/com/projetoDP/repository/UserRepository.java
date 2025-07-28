f8ececb3c5ec66e444cced70acc6a6eb55e35087
Thiago <ttf1880@gmail.com>
2025-07-28 17:23:03 -0300

    feat: cria UserRepository

--- src/main/java/br/com/projetoDP/repository/UserRepository.java
+++ src/main/java/br/com/projetoDP/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package br.com.projetoDP.repository;
 
-import br.com.projetoDP.entity.UserObserver;
+import br.com.projetoDP.entity.User;
 import br.com.projetoDP.utils.IBaseRepositoryBridge;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
@@ -8,14 +8,14 @@ import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
 
 @ApplicationScoped
-public class UserRepository implements IBaseRepositoryBridge<UserObserver> {
+public class UserRepository implements IBaseRepositoryBridge<User> {
 
     @PersistenceContext
     EntityManager em;
 
-    public UserObserver findByEmail(String email) {
+    public User findByEmail(String email) {
         try {
-            return em.createQuery("SELECT u FROM UserObserver u WHERE u.email = :email", UserObserver.class)
+            return em.createQuery("SELECT u FROM User u WHERE u.email = :email", User.class)
                     .setParameter("email", email)
                     .getSingleResult();
         } catch (NoResultException e) {
