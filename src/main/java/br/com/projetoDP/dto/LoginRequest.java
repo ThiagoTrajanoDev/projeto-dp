dbe7c902b0079b1b3d80faced63b37c2a34d30b5
Thiago <ttf1880@gmail.com>
2025-07-28 16:50:00 -0300

    feat: cria dto para login

---
+++ src/main/java/br/com/projetoDP/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package br.com.projetoDP.dto;
+
+public class LoginRequest {
+    public String email;
+    public String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
